91ac3db4d7253a57dd4c4a6b741a61dda2beaa61
Bryce Borer <bryce.borer@gmail.com>
2023-09-04 15:59:47 -0400

    Bio comment section is done (Avatar pictures are working properly)

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,18 +1,19 @@
+avatarPic = 0;
 const comments = [
     {
-        avatar: '',
+        avatar: 1,
         name: 'Miles Acosta',
         date: "20/12/2020",
         commentText: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     },
     {
-        avatar: '',
+        avatar: 1,
         name: 'Emilie Beach',
         date: "09/01/2021",
         commentText: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
     },
     {
-        avatar: '',
+        avatar: 1,
         name: 'Conner Walton',
         date: "17/02/2021",
         commentText: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
@@ -22,16 +23,29 @@ const comments = [
 
 function createCommentCard(comment) {
     
+    // The wrapper contains each comment card
     const wrapperEl = document.createElement('div');
     wrapperEl.classList.add('comments--wrapper');
 
-    const avatarEl = document.createElement('div');
-    avatarEl.classList.add('avatar');
-    // wrapperEl.appendChild(avatarEl);
+    // This is for the little avatar on the left
 
+    const avatarEl = document.createElement('img');
 
+    // gives new comments an avatar picture
+    if (comment.avatar !== 1){
+        avatarEl.src="./assets/images/Mohan-muruge.jpg";
+    }
+
+    avatarEl.classList.add('avatar__posted');
+    wrapperEl.appendChild(avatarEl);
+
+    // This is for all the text in the comment card
     const cardEl = document.createElement('div');
     cardEl.classList.add('comment-card');
+
+    // Makes the name and date have a space between them, and placed above the comment text
+    const cardTopEl = document.createElement('div');
+    cardTopEl.classList.add('comment-card__top');
     
     const dateEl = document.createElement('h4')
 
@@ -43,22 +57,25 @@ function createCommentCard(comment) {
         comment.date = `${month}/${day}/${year}`;
     }
 
-    console.log(comment.date)
     dateEl.innerText = comment.date;
     dateEl.classList.add('comment-card__top');
 
     const nameEl = document.createElement('h3');
     nameEl.innerText = comment.name;
     nameEl.classList.add('comment-card__top');
+    
 
     const commentEl = document.createElement('h4');
     commentEl.innerText = comment.commentText;
+    commentEl.classList.add('comment-card--posted')
 
     const dividerEl = document.createElement('div');
     dividerEl.classList.add('card-divider');
 
-    cardEl.appendChild(nameEl);
-    cardEl.appendChild(dateEl);
+
+    cardTopEl.appendChild(nameEl);
+    cardTopEl.appendChild(dateEl);
+    cardEl.appendChild(cardTopEl);
     cardEl.appendChild(commentEl);
     cardEl.appendChild(dividerEl);
 
@@ -72,7 +89,7 @@ function displayComment() {
 
     // Clear the comments div first
     myCommentsEl.innerHTML = "";
-
+    
     // Outputs comments, chronologically
     for (let i = comments.length-1; i >=0 ; i--) {
         const card = createCommentCard(comments[i]);
@@ -100,6 +117,7 @@ function handlePostSubmit(event) {
         commentText
     }
 
+    
     comments.push(newComment)
     displayComment();
     event.target.name.value = '';
