9d76fe38884256394369891f24311c23c909ae2e
Bryce Borer <bryce.borer@gmail.com>
2023-09-05 19:08:11 -0400

    started the date diver deeper... might need to skip this

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -105,11 +105,14 @@ function handlePostSubmit(event) {
     let name = event.target.name.value
     let commentText = event.target.comments.value
 
-    const date = new Date();
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    let currentDate = `${month}/${day}/${year}`;
+    // const date = new Date();
+    // let day = date.getDate();
+    // let month = date.getMonth() + 1;
+    // let year = date.getFullYear();
+    // let currentDate = `${month}/${day}/${year}`;
+
+    let currentDate;
+
 
     const newComment = {
         name,
@@ -127,5 +130,35 @@ function handlePostSubmit(event) {
 
 const myForm = document.getElementById('comment-section');
 myForm.addEventListener("submit", handlePostSubmit);
+
+function timeSince(date) {
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var interval = seconds / 31536000;
+    if (interval > 1) {
+      return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes ago";
+    }
+    // return Math.floor(seconds) + " seconds";
+    return "just now";
+  }
+  var aDay = 24*60*60*1000;
+  console.log(timeSince(new Date(Date.now())));
+  console.log(timeSince(new Date(Date.now()-aDay)));
+  console.log(timeSince(new Date(Date.now()-aDay*2)));
 
 displayComment();
