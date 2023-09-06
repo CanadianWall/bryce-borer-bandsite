eb21313975ec02a126ab85b41d18a632b15c1be9
Bryce Borer <bryce.borer@gmail.com>
2023-09-05 20:02:29 -0400

    Almost had the date diving deeper done, but ran out of time

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,4 +1,6 @@
-avatarPic = 0;
+// let currentDate = new Array();
+// let actualDate = new Array();
+// let dateCount = 0;
 const comments = [
     {
         avatar: 1,
@@ -49,6 +51,14 @@ function createCommentCard(comment) {
 
     const dateEl = document.createElement('h4')
 
+    // if(typeof(comment.date)==='string'){
+    //     dateEl.innerText = comment.date;
+    // }else{
+    //     actualDate[dateCount]= timeSince(currentDate[dateCount]);
+    //     dateEl.innerText = comment.actualDate[dateCount];
+    // }
+    
+    
     if (typeof comment.date === 'undefined') {
         const newDate = new Date();
         let day = newDate.getDate();
@@ -56,7 +66,7 @@ function createCommentCard(comment) {
         let year = newDate.getFullYear();
         comment.date = `${month}/${day}/${year}`;
     }
-
+    
     dateEl.innerText = comment.date;
     dateEl.classList.add('comment-card__top');
 
@@ -122,14 +132,14 @@ function handlePostSubmit(event) {
     if (commentText === '' || name === '') {
         return;
     }
-
+    // currentDate[dateCount] = timeSince(Date.now());
+    // currentDate[dateCount] = new Date(Date.now());
     const newComment = {
         name,
         currentDate,
         commentText
     }
-
-    console.log(name);
+    
     comments.push(newComment)
     displayComment();
     event.target.name.value = '';
@@ -140,34 +150,30 @@ function handlePostSubmit(event) {
 const myForm = document.getElementById('comment-section');
 myForm.addEventListener("submit", handlePostSubmit);
 
-function timeSince(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
-    var interval = seconds / 31536000;
-    if (interval > 1) {
-        return Math.floor(interval) + " years";
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-        return Math.floor(interval) + " months";
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-        return Math.floor(interval) + " days";
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-        return Math.floor(interval) + " hours ago";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-        return Math.floor(interval) + " minutes ago";
-    }
-    // return Math.floor(seconds) + " seconds";
-    return "just now";
-}
-var aDay = 24 * 60 * 60 * 1000;
-console.log(timeSince(new Date(Date.now())));
-console.log(timeSince(new Date(Date.now() - aDay)));
-console.log(timeSince(new Date(Date.now() - aDay * 2)));
+// function timeSince(date) {
+//     var seconds = Math.floor((new Date() - date) / 1000);
+//     var interval = seconds / 31536000;
+//     if (interval > 1) {
+//         return Math.floor(interval) + " years";
+//     }
+//     interval = seconds / 2592000;
+//     if (interval > 1) {
+//         return Math.floor(interval) + " months";
+//     }
+//     interval = seconds / 86400;
+//     if (interval > 1) {
+//         return Math.floor(interval) + " days";
+//     }
+//     interval = seconds / 3600;
+//     if (interval > 1) {
+//         return Math.floor(interval) + " hours ago";
+//     }
+//     interval = seconds / 60;
+//     if (interval > 1) {
+//         return Math.floor(interval) + " minutes ago";
+//     }
+//     return Math.floor(seconds) + " seconds ago";
+// }
+
 
 displayComment();
