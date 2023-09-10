58efd9a3cf440cd325c848697b3a234f104b890a
Bryce Borer <bryce.borer@gmail.com>
2023-09-10 18:23:29 -0400

    Good start on the shows page

--- scripts/shows-page.js
+++ scripts/shows-page.js
@@ -1,133 +1,141 @@
+// API Server
+let baseUrl = "https://project-1-api.herokuapp.com";
 
-const shows = [
-    {
-        date: 'Wed Dec 15 2021',
-        venue: 'Press Club',
-        location: "San Francisco, CA"
-    },
-    {
-        date: 'Fri Nov 26 2021',
-        venue: 'Moscow Center',
-        location: "San Francisco, CA"
-    },
-    {
-        date: 'Sat Nov 06 2021',
-        venue: 'Hyatt Agency',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Fri Oct 15 2021',
-        venue: 'View Lounge',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Tue Sept 21 2021',
-        venue: 'Pier 3 East ',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Mon Sept 06 2021',
-        venue: 'Ronald Lane',
-        location: 'San Francisco, CA'
-    }
-];
+// let api_key = '?api_key=6943b8bd-d12b-46e7-8f57-c03dd24e34b5';
+let api_key = '?api_key=6943b8bd-d12b-46e7-8f57-c03dd24e34b5';
 
-function createShowCard(show, i) {
-    //initialize a card
-    const cardEl = document.createElement('article');
-    cardEl.classList.add('show');
-    cardEl.setAttribute('id', i);
+let showsEndpoint = "/showdates";
 
-    const dateGroup = document.createElement('div');
-    dateGroup.classList.add('show__groups');
-
-    const dateHeading = document.createElement('h4'); // is a node
-    dateHeading.innerHTML = 'DATE';
-    dateHeading.classList.add('shows--heading');
-
-    const dateEl = document.createElement('h4');
-    dateEl.innerText = show.date;
-    dateEl.classList.add('demi');
-    dateEl.classList.add('shows__item');
-
-    const venueGroup = document.createElement('div');
-    venueGroup.classList.add('show__groups');
-
-    const venueHeading = document.createElement('h4'); // is a node
-    venueHeading.innerHTML = 'VENUE';
-    venueHeading.classList.add('shows--heading');
-
-    const venueEl = document.createElement('h4');
-    venueEl.innerText = show.venue;
-    venueEl.classList.add('shows__item');
-
-    const locationGroup = document.createElement('div');
-    locationGroup.classList.add('show__groups');
-
-    const locationHeading = document.createElement('h4'); // is a node
-    locationHeading.innerHTML = 'LOCATION';
-    locationHeading.classList.add('shows--heading');
-
-    const locationEl = document.createElement('h4');
-    locationEl.innerText = show.location;
-    locationEl.classList.add('shows__item');
-
-    buttonId = i+10;
-    const buttonEl = document.createElement('button');
-    buttonEl.innerText = 'BUY TICKETS';
-    buttonEl.classList.add('button--shows');
-    buttonEl.setAttribute('id', buttonId);
-
-    if (screen.width < 768 || ((screen.width >= 768) && i === 5)) {
-        dateGroup.appendChild(dateHeading);
-        venueGroup.appendChild(venueHeading);
-        locationGroup.appendChild(locationHeading);
-    }
-
-    dateGroup.appendChild(dateEl);
-    venueGroup.appendChild(venueEl);
-    locationGroup.appendChild(locationEl);
-
-    cardEl.appendChild(dateGroup);
-    cardEl.appendChild(venueGroup);
-    cardEl.appendChild(locationGroup);
-    cardEl.appendChild(buttonEl);
-
-    return cardEl;
-}
-
-function displayShow() {
+const getShows = () => {
     const myShowsEl = document.querySelector("#showsId");
-
-    // Clear the comments div first
     myShowsEl.innerHTML = "";
+    axios.get(`${baseUrl}${showsEndpoint}${api_key}`) //this give me back a promise
+        .then((result) => {
+            console.log(result.data);
+            result.data.forEach((showTicket) => {
+                //initialize a card
+                const cardEl = document.createElement('article');
+                cardEl.classList.add('show');
+                // cardEl.setAttribute('id', i);
 
-    // Outputs comments, chronologically
-    for (let i = shows.length - 1; i >= 0; i--) {
-        const card = createShowCard(shows[i], i);
-        myShowsEl.appendChild(card);
-    }
+                const dateGroup = document.createElement('div');
+                dateGroup.classList.add('show__groups');
 
+                const dateHeading = document.createElement('h4'); // is a node
+                dateHeading.innerHTML = 'DATE';
+                dateHeading.classList.add('shows--heading');
+
+                const dateEl = document.createElement('h4');
+                dateEl.textContent = timeSince(showTicket.timestamp);
+
+                dateEl.classList.add('demi');
+                dateEl.classList.add('shows__item');
+
+                const venueGroup = document.createElement('div');
+                venueGroup.classList.add('show__groups');
+
+                const venueHeading = document.createElement('h4'); // is a node
+                venueHeading.innerHTML = 'VENUE';
+                venueHeading.classList.add('shows--heading');
+
+                const venueEl = document.createElement('h4');
+                venueEl.textContent = showTicket.place;
+                venueEl.classList.add('shows__item');
+
+                const locationGroup = document.createElement('div');
+                locationGroup.classList.add('show__groups');
+
+                const locationHeading = document.createElement('h4'); // is a node
+                locationHeading.textContent = 'LOCATION';
+                locationHeading.classList.add('shows--heading');
+
+                const locationEl = document.createElement('h4');
+                locationEl.textContent = showTicket.location;
+                locationEl.classList.add('shows__item');
+
+                // buttonId = i + 10;
+                const buttonEl = document.createElement('button');
+                buttonEl.innerText = 'BUY TICKETS';
+                buttonEl.classList.add('button--shows');
+                // buttonEl.setAttribute('id', buttonId);
+
+                if (screen.width < 768 || ((screen.width >= 768) && i === 5)) {
+                    dateGroup.appendChild(dateHeading);
+                    venueGroup.appendChild(venueHeading);
+                    locationGroup.appendChild(locationHeading);
+                }
+
+                dateGroup.appendChild(dateEl);
+                venueGroup.appendChild(venueEl);
+                locationGroup.appendChild(locationEl);
+
+                cardEl.appendChild(dateGroup);
+                cardEl.appendChild(venueGroup);
+                cardEl.appendChild(locationGroup);
+                cardEl.appendChild(buttonEl);
+                myShowsEl.appendChild(cardEl);
+            })
+        })
+        .catch((error) => console.log(error));
 }
+
+// function displayShow() {
+//     const myShowsEl = document.querySelector("#showsId");
+
+//     // Clear the comments div first
+//     myShowsEl.innerHTML = "";
+
+//     // Outputs comments, chronologically
+//     for (let i = shows.length - 1; i >= 0; i--) {
+//         const card = createShowCard(shows[i], i);
+//         myShowsEl.appendChild(card);
+//     }
+
+// }
+
 
 // this reverts the color to white on all rows, except darkens the "selected" row
-function selectedShow(activeShow){
-    console.log("active " + activeShow)
+// function selectedShow(activeShow){
+//     console.log("active " + activeShow)
     
-    //this statement converts the button id to the article (background) id
-    if (activeShow >=10){
-        activeShow -=10;
-        }
+//     //this statement converts the button id to the article (background) id
+//     if (activeShow >=10){
+//         activeShow -=10;
+//         }
 
-     for(i = 0; i<=5; i++){
-    document.getElementById(i).style.backgroundColor = "#FFFFFF";
-     }
-    document.getElementById(activeShow).style.backgroundColor = "#E1E1E1";
+//      for(i = 0; i<=5; i++){
+//     document.getElementById(i).style.backgroundColor = "#FFFFFF";
+//      }
+//     document.getElementById(activeShow).style.backgroundColor = "#E1E1E1";
+// }
+function timeSince(date) {
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var interval = seconds / 31536000;
+    if (interval > 1) {
+        return Math.floor(interval) + " years ago";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return Math.floor(interval) + " months ago";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return Math.floor(interval) + " days ago";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return Math.floor(interval) + " hours ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
 }
     
 
 
-displayShow();
+
 
 // Shows will be rerendered when the screen size changes
 window.addEventListener("resize", function (event) {
@@ -135,12 +143,14 @@ window.addEventListener("resize", function (event) {
 })
 
 //this listens for mouse clicks on the "selected" row on the show table
-for (i = 0; i<=5; i++){ 
-    document.getElementById(i).addEventListener('click', function(event){
-            selectedShow(event.target.id);
-    })
-    document.getElementById(i+10).addEventListener('click', function(event){
-            selectedShow(event.target.id);
-    })
-}
+// for (i = 0; i<=5; i++){ 
+//     document.getElementById(i).addEventListener('click', function(event){
+//             selectedShow(event.target.id);
+//     })
+//     document.getElementById(i+10).addEventListener('click', function(event){
+//             selectedShow(event.target.id);
+//     })
+// }
 
+// displayShow();
+getShows();
--- scripts/shows-page2.js
+++ scripts/shows-page2.js
@@ -1,5 +1,148 @@
-// "api_key": "6943b8bd-d12b-46e7-8f57-c03dd24e34b5"
-// https://project-1-api.herokuapp.com?api_key=6943b8bd-d12b-46e7-8f57-c03dd24e34b5
+
+const shows = [
+    {
+        date: 'Wed Dec 15 2021',
+        venue: 'Press Club',
+        location: "San Francisco, CA"
+    },
+    {
+        date: 'Fri Nov 26 2021',
+        venue: 'Moscow Center',
+        location: "San Francisco, CA"
+    },
+    {
+        date: 'Sat Nov 06 2021',
+        venue: 'Hyatt Agency',
+        location: 'San Francisco, CA'
+    },
+    {
+        date: 'Fri Oct 15 2021',
+        venue: 'View Lounge',
+        location: 'San Francisco, CA'
+    },
+    {
+        date: 'Tue Sept 21 2021',
+        venue: 'Pier 3 East ',
+        location: 'San Francisco, CA'
+    },
+    {
+        date: 'Mon Sept 06 2021',
+        venue: 'Ronald Lane',
+        location: 'San Francisco, CA'
+    }
+];
+
+function createShowCard(show, i) {
+    //initialize a card
+    const cardEl = document.createElement('article');
+    cardEl.classList.add('show');
+    cardEl.setAttribute('id', i);
+
+    const dateGroup = document.createElement('div');
+    dateGroup.classList.add('show__groups');
+
+    const dateHeading = document.createElement('h4'); // is a node
+    dateHeading.innerHTML = 'DATE';
+    dateHeading.classList.add('shows--heading');
+
+    const dateEl = document.createElement('h4');
+    dateEl.innerText = show.date;
+    dateEl.classList.add('demi');
+    dateEl.classList.add('shows__item');
+
+    const venueGroup = document.createElement('div');
+    venueGroup.classList.add('show__groups');
+
+    const venueHeading = document.createElement('h4'); // is a node
+    venueHeading.innerHTML = 'VENUE';
+    venueHeading.classList.add('shows--heading');
+
+    const venueEl = document.createElement('h4');
+    venueEl.innerText = show.venue;
+    venueEl.classList.add('shows__item');
+
+    const locationGroup = document.createElement('div');
+    locationGroup.classList.add('show__groups');
+
+    const locationHeading = document.createElement('h4'); // is a node
+    locationHeading.innerHTML = 'LOCATION';
+    locationHeading.classList.add('shows--heading');
+
+    const locationEl = document.createElement('h4');
+    locationEl.innerText = show.location;
+    locationEl.classList.add('shows__item');
+
+    buttonId = i+10;
+    const buttonEl = document.createElement('button');
+    buttonEl.innerText = 'BUY TICKETS';
+    buttonEl.classList.add('button--shows');
+    buttonEl.setAttribute('id', buttonId);
+
+    if (screen.width < 768 || ((screen.width >= 768) && i === 5)) {
+        dateGroup.appendChild(dateHeading);
+        venueGroup.appendChild(venueHeading);
+        locationGroup.appendChild(locationHeading);
+    }
+
+    dateGroup.appendChild(dateEl);
+    venueGroup.appendChild(venueEl);
+    locationGroup.appendChild(locationEl);
+
+    cardEl.appendChild(dateGroup);
+    cardEl.appendChild(venueGroup);
+    cardEl.appendChild(locationGroup);
+    cardEl.appendChild(buttonEl);
+
+    return cardEl;
+}
+
+function displayShow() {
+    const myShowsEl = document.querySelector("#showsId");
+
+    // Clear the comments div first
+    myShowsEl.innerHTML = "";
+
+    // Outputs comments, chronologically
+    for (let i = shows.length - 1; i >= 0; i--) {
+        const card = createShowCard(shows[i], i);
+        myShowsEl.appendChild(card);
+    }
+
+}
+
+// this reverts the color to white on all rows, except darkens the "selected" row
+function selectedShow(activeShow){
+    console.log("active " + activeShow)
+    
+    //this statement converts the button id to the article (background) id
+    if (activeShow >=10){
+        activeShow -=10;
+        }
+
+     for(i = 0; i<=5; i++){
+    document.getElementById(i).style.backgroundColor = "#FFFFFF";
+     }
+    document.getElementById(activeShow).style.backgroundColor = "#E1E1E1";
+}
+    
+
+
+displayShow();
+
+// Shows will be rerendered when the screen size changes
+window.addEventListener("resize", function (event) {
+    location.reload();
+})
+
+//this listens for mouse clicks on the "selected" row on the show table
+for (i = 0; i<=5; i++){ 
+    document.getElementById(i).addEventListener('click', function(event){
+            selectedShow(event.target.id);
+    })
+    document.getElementById(i+10).addEventListener('click', function(event){
+            selectedShow(event.target.id);
+    })
+}
 
 // API Server
 let baseUrl = "https://project-1-api.herokuapp.com";
@@ -11,10 +154,10 @@ let api_key = '?api_key=6943b8bd-d12b-46e7-8f57-c03dd24e34b5';
 let commentsEndpoint = "/comments";
 let showsEndpoint = "/showdates";
 
-const getComments = () => {
+const getShows = () => {
     const myCommentsEl = document.querySelector("#posted--comment");
                 myCommentsEl.innerHTML = "";
-    axios.get(`${baseUrl}${commentsEndpoint}${api_key}`) //this give me back a promise
+    axios.get(`${baseUrl}${showsEndpoint}${api_key}`) //this give me back a promise
         .then((result) => {
             console.log(result.data);
             let objectIndex = 0; // for sorting comments
@@ -119,7 +262,6 @@ function timeSince(date) {
     }
     return Math.floor(seconds) + " seconds ago";
 }
-
 function handlePostSubmit(event) {
     event.preventDefault();
 
@@ -153,10 +295,10 @@ function handlePostSubmit(event) {
     .then((response) => console.log(response))
     .catch((error) => console.log(error))
 
-    // Verifies POST is complete, then runs getComments()
+    // Verifies POST is complete, then runs getShows()
     let postCheck = axios.get(`${baseUrl}${commentsEndpoint}${api_key}`)
     .then((response) => this.data = response.data)
-    postCheck.then(() => {getComments();})
+    postCheck.then(() => {getShows();})
 
     //clears the textboxes
     event.target.name.value = '';
@@ -164,7 +306,4 @@ function handlePostSubmit(event) {
 }
 
 
-getComments()
-const myForm = document.getElementById('comment-section');
-myForm.addEventListener("submit", handlePostSubmit);
-
+getShows()
